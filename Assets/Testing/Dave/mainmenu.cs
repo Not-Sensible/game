894668ddb93f91627ca9529915b2b10bc246b536
dave bentley <david.bentley94@btinternet.com>
2018-01-24 19:53:02 +0000

    main menu
    Monumetaly messed it up and had to restart the menu however bacground is in and one button is there that i need to sort out

---
+++ Assets/Testing/Dave/mainmenu.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class mainmenu : MonoBehaviour {
+
+    public Texture background;
+
+    private void OnGUI()
+    {
+        GUI.DrawTexture(new Rect(0, 0, Screen.width, Screen.height), background);
+
+      if(GUI.Button(new Rect(Screen.width * .5f, Screen.height * .5f, Screen.width * .5f, Screen.height * .1f), "Play"))
+        {
+
+        }
+    }
+}

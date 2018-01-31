7f24976fa43b35e2bb22b8fe62f68d6a4439db76
dave bentley <david.bentley94@btinternet.com>
2018-01-31 13:19:59 +0000

    menus
    all main menu buttons added and started on the level select menu

---
+++ Assets/Testing/Dave/levelSelect.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class levelSelect : MonoBehaviour {
+
+    public Texture background;
+    public GUIStyle level1;
+    public GUIStyle level2;
+    public GUIStyle level3;
+    public GUIStyle level4;
+    public GUIStyle level5;
+    public GUIStyle back;
+
+    public float level1Y;
+    public float level2Y;
+    public float level3Y;
+    public float level4Y;
+    public float level5Y;
+    public float backY;
+
+    public float level1X;
+    public float level2X;
+    public float level3X;
+    public float level4X;
+    public float level5X;
+    public float backX;
+
+    void OnGUI() { 
+    GUI.DrawTexture(new Rect(0, 0, Screen.width, Screen.height), background);
+
+        if(GUI.Button(new Rect(Screen.width* level1X, Screen.height* level1Y, Screen.width* .25f, Screen.height* .075f), "Level1", level1))
+        {
+          
+            SceneManager.LoadScene("Actual_level");
+            Debug.Log("working");
+        }
+        if (GUI.Button(new Rect(Screen.width* level2X, Screen.height* level2Y, Screen.width* .25f, Screen.height* .075f), "Level2", level2))
+        {
+
+        }
+        if (GUI.Button(new Rect(Screen.width* level3X, Screen.height* level3Y, Screen.width* .25f, Screen.height* .075f), "Level3", level3))
+        {
+
+        }
+        if (GUI.Button(new Rect(Screen.width* level4X, Screen.height* level4Y, Screen.width* .25f, Screen.height* .075f), "Level4", level4))
+        {
+           
+        }
+        if (GUI.Button(new Rect(Screen.width * level5X, Screen.height * level5Y, Screen.width * .25f, Screen.height * .075f), "Level5", level5))
+        {
+
+        }
+        if (GUI.Button(new Rect(Screen.width * backX, Screen.height * backY, Screen.width * .25f, Screen.height * .075f), "Back", back))
+        {
+
+        }
+    }
+
+}
+
--- Assets/Testing/Dave/mainmenu.cs
+++ Assets/Testing/Dave/mainmenu.cs
@@ -1,27 +1,48 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.SceneManagement;
 public class mainmenu : MonoBehaviour {
 
     public Texture background;
-    public GUIStyle button1;
+    public GUIStyle levelSelect;
+    public GUIStyle newGame;
+    public GUIStyle options;
+    public GUIStyle credits;
+    public GUIStyle quit;
 
-    public float buttonY;
-    public float button2Y;
-    public float buttonX;
-    public float button2X;
+    public float levelSelectY;
+    public float newGameY;
+    public float optionsY;
+    public float creditsY;
+    public float quitY;
+
+    public float levelSelectX;
+    public float newGameX;
+    public float optionsX;
+    public float creditsX;
+    public float quitX;
+
+
     void OnGUI()
     {
         GUI.DrawTexture(new Rect(0, 0, Screen.width, Screen.height), background);
 
-      if(GUI.Button(new Rect(Screen.width * buttonX, Screen.height * buttonY, Screen.width * .25f, Screen.height * .075f), "Level Select",button1))
+        if(GUI.Button(new Rect(Screen.width * levelSelectX, Screen.height * levelSelectY, Screen.width * .25f, Screen.height * .075f), "Level Select", levelSelect))
+        {
+            SceneManager.LoadScene(1);
+        }
+        if (GUI.Button(new Rect(Screen.width * newGameX, Screen.height * newGameY, Screen.width * .25f, Screen.height * .075f), "New Game", levelSelect))
         {
 
         }
-        if (GUI.Button(new Rect(Screen.width * button2X, Screen.height * button2Y, Screen.width * .25f, Screen.height * .075f), "Credits", button1))
+        if (GUI.Button(new Rect(Screen.width * optionsX, Screen.height * optionsY, Screen.width * .25f, Screen.height * .075f), "Options", levelSelect))
         {
 
+        }
+        if (GUI.Button(new Rect(Screen.width * quitX, Screen.height * quitY, Screen.width * .25f, Screen.height * .075f), "Quit", levelSelect))
+        {
+            Application.Quit();
         }
     }
 }

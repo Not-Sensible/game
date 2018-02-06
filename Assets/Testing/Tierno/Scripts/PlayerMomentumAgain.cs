20641f891d92dc1b0614311da23a596316e83c8e
Tiernan <tiernanmccarthy@outlook.com>
2018-02-06 21:43:35 +0000

    Player controlled momentum is in.
    Work is needed on terrain movement. Perhaps a form of magnesis is needed.

--- Assets/Testing/Tierno/Scripts/PlayerMomentumAgain.cs
+++ Assets/Testing/Tierno/Scripts/PlayerMomentumAgain.cs
@@ -12,9 +12,10 @@ public class PlayerMomentumAgain : MonoBehaviour {
     private Animator animy;
     private bool playermoving;
     public LayerMask CollideList; //Temporary, dimension shifting will require lots of these, although I can imagine more blunt ways of doing it
-    public float FallRate; //How fast the player will fall down slopes
+    private float groundslide = 12;
     public float GroundCheckRadius;
-    private char direction;
+    private char direction; //Actual direction of the player
+    private char DesiredDir; //The desired direction
     public float slowdown;
     private float timeLeft;
     private Quaternion[] angles;
@@ -66,29 +67,32 @@ public class PlayerMomentumAgain : MonoBehaviour {
 
     }
 
-    void Movement()
+    void InputScript()
     {
         //Placeholder
         if (Input.GetKeyDown("a"))
         {
-            X = -speed;
+            DesiredDir = 'L';
             playermoving = true;
         }
-        else if (Input.GetKeyDown("d"))
+        if (Input.GetKeyDown("d"))
         {
-            X = speed;
+            DesiredDir = 'R';
             playermoving = true;
         }
         if (Input.GetKeyUp("a"))
         {
-           // X = 0;
+            // X = 0;
+            DesiredDir = 'N';
             playermoving = false;
         }
-        else if (Input.GetKeyUp("d"))
+        if (Input.GetKeyUp("d"))
         {
             //X = 0;
+            DesiredDir = 'N';
             playermoving = false;
         }
+        Movement(DesiredDir, onGround);
         Y = -GravityStrength;
         transform.Translate(X * Time.deltaTime, 0, 0);
         if (onGround != true)
@@ -97,40 +101,54 @@ public class PlayerMomentumAgain : MonoBehaviour {
         }
 
     }
+    void Movement(char dir,bool IsOnGround)
+    {
+        if (onGround == true && dir == 'L' && X > -RealMaxspeed)
+            X += -speed * Time.deltaTime;
+        else if (onGround == true && dir == 'R' && X < RealMaxspeed)
+            X += speed * Time.deltaTime;
+
+    }
 
     void TerrainSlowDown(float slowdownlocal)
     {
 
-        if (transform.rotation.z > 0)
-            X += -slowdownlocal * Time.deltaTime;
+        if (transform.rotation.z > 0) //If the player's Direction is Right or Left
+            X += -slowdownlocal * Time.deltaTime;  //If the Player is climbing right, they should start falling left
         else
-            X += slowdownlocal * Time.deltaTime;
+            X += slowdownlocal * Time.deltaTime; //If the Player is climbing Left, they should start falling right
 
-        
+
 
     }
 
     void Momentum()
     {
         //If the player is between 10 degrees and -10 degrees
-        if (transform.rotation.z < angles[0].z && transform.rotation.z > -angles[0].z)
+        if (onGround == true)
         {
-            RealMaxspeed = maxspeed;
-            if (playermoving == true)
+            if (DesiredDir == 'R' && X < 0 || DesiredDir == 'L' && X > 0)
             {
-                if (X > RealMaxspeed)   //If the player is moving at a speed faster than the maxspeed designated by the terrain, slow them down.
-                    X -= slowdown*Time.deltaTime;
-                else if(X<-RealMaxspeed)
-                    X += slowdown*Time.deltaTime;
-
+                playermoving = false;
             }
-            else if (playermoving!=true)
+            if (transform.rotation.z < angles[0].z && transform.rotation.z > -angles[0].z)
             {
+                RealMaxspeed = maxspeed;
+                if (playermoving == true)
+                {
+                    if (X > RealMaxspeed)   //If the player is moving at a speed faster than the maxspeed designated by the terrain, slow them down.
+                        X -= groundslide * Time.deltaTime;
+                    else if (X < -RealMaxspeed)
+                        X += groundslide * Time.deltaTime;
+
+                }
+                else if (playermoving != true)
+                {
                     if (X > 0)
                     {
                         if (X - 0.1f > 0f)
                         {
-                            X += -slowdown*Time.deltaTime;
+                            X += -groundslide * Time.deltaTime;
                         }
                         else
                             X = 0;
@@ -139,44 +157,65 @@ public class PlayerMomentumAgain : MonoBehaviour {
                     {
                         if (X + 0.1f < 0f)
                         {
-                            X += slowdown* Time.deltaTime;
+                            X += groundslide * Time.deltaTime;
                         }
                         else
                             X = 0;
                     }
-                
+
+                }
+
+
+
+
             }
 
-        
+            //Behaviours for all the different angles go here.
+            if (transform.rotation.z >= angles[0].z && transform.rotation.z <= angles[1].z || transform.rotation.z <= -angles[0].z && transform.rotation.z >= -angles[1].z) //10 degrees
+            {
+                TerrainSlowDown(slowdown * 1.1f);
 
+            }
+            else if (transform.rotation.z >= angles[1].z && transform.rotation.z <= angles[2].z || transform.rotation.z <= -angles[0].z && transform.rotation.z >= -angles[1].z) //20
+            {
+                TerrainSlowDown(slowdown * 1.2f);
 
-        }
+            }
+            else if (transform.rotation.z >= angles[2].z && transform.rotation.z <= angles[3].z || transform.rotation.z <= -angles[1].z && transform.rotation.z >= -angles[2].z) //30
+            {
+                TerrainSlowDown(slowdown * 1.3f);
 
-        //Behaviours for all the different angles go here.
-        if(transform.rotation.z>=angles[0].z && transform.rotation.z <=angles[1].z || transform.rotation.z <= -angles[0].z && transform.rotation.z >= -angles[1].z)
-        {
-            TerrainSlowDown(slowdown * 1.1f);
+            }
+            else if (transform.rotation.z >= angles[3].z && transform.rotation.z <= angles[4].z || transform.rotation.z <= -angles[2].z && transform.rotation.z >= -angles[3].z) //40
+            {
+                TerrainSlowDown(slowdown * 1.4f);
 
-        }
-        else if (transform.rotation.z >= angles[1].z && transform.rotation.z <= angles[2].z || transform.rotation.z <= -angles[0].z && transform.rotation.z >= -angles[1].z)
-        {
-            TerrainSlowDown(slowdown * 1.2f);
+            }
+            else if (transform.rotation.z >= angles[4].z && transform.rotation.z <= angles[5].z || transform.rotation.z <= -angles[4].z && transform.rotation.z >= -angles[5].z) //50
+            {
+                TerrainSlowDown(slowdown * 1.5f);
 
-        }
-        else if (transform.rotation.z >= angles[2].z && transform.rotation.z <= angles[3].z || transform.rotation.z <= -angles[1].z && transform.rotation.z >= -angles[2].z)
-        {
-            TerrainSlowDown(slowdown * 1.3f);
+            }
+            else if (transform.rotation.z >= angles[5].z && transform.rotation.z <= angles[6].z || transform.rotation.z <= -angles[5].z && transform.rotation.z >= -angles[6].z) //60
+            {
+                TerrainSlowDown(slowdown * 1.6f);
 
-        }
-        else if (transform.rotation.z >= angles[3].z && transform.rotation.z <= angles[4].z || transform.rotation.z <= -angles[2].z && transform.rotation.z >= -angles[3].z)
-        {
-            TerrainSlowDown(slowdown * 1.4f);
+            }
+            else if (transform.rotation.z >= angles[6].z && transform.rotation.z <= angles[7].z || transform.rotation.z <= -angles[6].z && transform.rotation.z >= -angles[7].z) //70
+            {
+                TerrainSlowDown(slowdown * 1.7f);
 
-        }
-        else if (transform.rotation.z >= angles[4].z && transform.rotation.z <= angles[5].z || transform.rotation.z >= -angles[3].z && transform.rotation.z <= -angles[4].z)
-        {
-            TerrainSlowDown(slowdown * 1.5f);
+            }
+            else if (transform.rotation.z >= angles[7].z && transform.rotation.z <= angles[8].z || transform.rotation.z <= -angles[7].z && transform.rotation.z >= -angles[8].z) //80
+            {
+                TerrainSlowDown(slowdown * 1.8f);
 
+            }
+            else if (transform.rotation.z >= angles[8].z && transform.rotation.z <= angles[9].z || transform.rotation.z <= -angles[8].z && transform.rotation.z >= -angles[9].z) //90
+            {
+                TerrainSlowDown(slowdown * 1.9f);
+
+            }
         }
     }
 
@@ -186,7 +225,7 @@ public class PlayerMomentumAgain : MonoBehaviour {
 
     // Update is called once per frame
     void Update () {
-        Movement();
+        InputScript();
         Momentum();
 	}
     void FixedUpdate()

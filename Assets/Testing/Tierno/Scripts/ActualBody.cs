bb91cfa7543459dec3afc71b86ad5521ba034cb7
Tiernan <tiernanmccarthy@outlook.com>
2018-04-10 23:27:07 +0100

    Character body flipped properly just needs animations
    safsfasfa

---
+++ Assets/Testing/Tierno/Scripts/ActualBody.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ActualBody : MonoBehaviour {
+    private PlayerMomentumAgain player;
+    char dir = 'R';
+    SpriteRenderer m_SpriteRenderer;
+    public Sprite sprite1;
+    public Sprite sprite2;
+    // Use this for initialization
+    void Start () {
+        player = FindObjectOfType<PlayerMomentumAgain>();
+        //Fetch the SpriteRenderer of the Sprite
+        m_SpriteRenderer = GetComponent<SpriteRenderer>();
+        m_SpriteRenderer.sprite = sprite1;
+        //Output the current Texture of the Sprite (this returns the source Sprite if the Texture isn't packed)
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+     void FixedUpdate()
+    {
+        if (player.DesiredDir == 'R' && dir != 'R')
+        {
+            m_SpriteRenderer.sprite = sprite1;
+            dir = 'R';
+        }
+        else if (player.DesiredDir == 'L' && dir != 'L')
+        {
+            m_SpriteRenderer.sprite = sprite2;
+            dir = 'L';
+        }
+    }
+}

631e50928ef7335e4d3128f3cb319dd0f0d9d7ef
dave bentley <david.bentley94@btinternet.com>
2018-04-18 13:56:39 +0100

    Adding button images to main menu

--- Assets/Testing/Dave/mainmenu.cs
+++ Assets/Testing/Dave/mainmenu.cs
@@ -7,19 +7,19 @@ using UnityEngine.EventSystems;
 public class mainmenu : MonoBehaviour {
 
     public Texture background;
-    public GUIStyle levelSelect;
+    //public GUIStyle levelSelect;
     public GUIStyle newGame;
-    public GUIStyle credits;
+    //public GUIStyle credits;
     public GUIStyle quit;
 
-    public float levelSelectY;
+    //public float levelSelectY;
     public float newGameY;
-    public float creditsY;
+    //public float creditsY;
     public float quitY;
 
-    public float levelSelectX;
+    //public float levelSelectX;
     public float newGameX;
-    public float creditsX;
+    //public float creditsX;
     public float quitX;
 
     
@@ -28,20 +28,20 @@ public class mainmenu : MonoBehaviour {
     {
         GUI.DrawTexture(new Rect(0, 0, Screen.width, Screen.height), background);
 
-        if(GUI.Button(new Rect(Screen.width * levelSelectX, Screen.height * levelSelectY, Screen.width * .25f, Screen.height * .075f), "Level Select", levelSelect))
+        /*if(GUI.Button(new Rect(Screen.width * levelSelectX, Screen.height * levelSelectY, Screen.width * .25f, Screen.height * .075f), "Level Select", levelSelect))
         {
             SceneManager.LoadScene(1);
             
-        }
-        if (GUI.Button(new Rect(Screen.width * newGameX, Screen.height * newGameY, Screen.width * .25f, Screen.height * .075f), "New Game", newGame))
+        }*/
+        if (GUI.Button(new Rect(Screen.width * newGameX, Screen.height * newGameY, Screen.width * .25f, Screen.height * .075f), "Start", newGame))
         {
-            SceneManager.LoadScene(2);
+            SceneManager.LoadScene(1);
         }
 
-        if(GUI.Button(new Rect(Screen.width * creditsX, Screen.height * creditsY, Screen.width * .25f, Screen.height * .075f), "Credits", credits))
+        /*if(GUI.Button(new Rect(Screen.width * creditsX, Screen.height * creditsY, Screen.width * .25f, Screen.height * .075f), "Credits", credits))
         {
             //SceneManager.LoadScene();
-        }
+        }*/
 
         if (GUI.Button(new Rect(Screen.width * quitX, Screen.height * quitY, Screen.width * .25f, Screen.height * .075f), "Quit", quit))
         {
